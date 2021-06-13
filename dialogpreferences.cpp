53bc1a6ede69465faec513f7922f8b4a7311c304
wybren1971 <wybren@vanduinenotten.nl>
2021-06-13 13:33:49 +0200

    Point to fluxengine file instead of dir

--- dialogpreferences.cpp
+++ dialogpreferences.cpp
@@ -8,8 +8,8 @@ DialogPreferences::DialogPreferences(QWidget *parent) :
 {
 
     ui->setupUi(this);
-
     QSettings settings("Fluxengine_GUI", "Fluxengine_GUI");
+    m_fluxengine.setWorkingDirectory(settings.value("fluxengine").toString());
 
     if (settings.value("datalocation").toString() != "")
     {
@@ -42,12 +42,16 @@ DialogPreferences::DialogPreferences(QWidget *parent) :
         }
     }
 
+    connect(&m_fluxengine,&fluxengine::output,this,&DialogPreferences::output);
+    connect(&m_fluxengine,&fluxengine::enableFluxengineCommands,this,&DialogPreferences::enablecommands);
     connect(ui->btndatalocation, SIGNAL(clicked()), SLOT(browse()));
     connect(ui->btnfluxlocation, SIGNAL(clicked()), SLOT(browseflux()));
     connect(ui->btnfluxenginelocation, SIGNAL(clicked()), SLOT(browsefluxengine()));
+    connect(ui->btnInitialize, SIGNAL(clicked()), SLOT(initializefluxengine()));
     connect(ui->buttonBox, SIGNAL(accepted()), SLOT(save()));
-    //ui->tabWidget->
-
+    ui->tabWidget->setTabVisible(2, false);
+    ui->tabWidget->setCurrentIndex(0);
+    State = 0;
 }
 
 DialogPreferences::~DialogPreferences()
@@ -71,7 +75,6 @@ void DialogPreferences::browsefluxengine()
                                 tr("Set default fluxengine application"), ui->cmbFluxengineLocation->currentText()));
     }
 
-    qInfo() << directory.absolutePath();
     if (directory.absolutePath() != "") {
         if (ui->cmbFluxengineLocation->findText(directory.absolutePath()) == -1)
             ui->cmbFluxengineLocation->addItem(directory.absolutePath());
@@ -117,6 +120,240 @@ void DialogPreferences::browse()
             ui->cmbdatalocation->addItem(directory);
         ui->cmbdatalocation->setCurrentIndex(ui->cmbdatalocation->findText(directory));
     }
+}
+
+void DialogPreferences::enablecommands(bool running)
+{
+    qInfo() << Q_FUNC_INFO;
+    qInfo() << running;
+    if (running)
+    {
+        //
+    }
+}
+void DialogPreferences::initializefluxengine()
+{
+    qInfo() << Q_FUNC_INFO;
+    qInfo() << State;
+    switch (State) {
+        case 0:
+            m_fluxengine.setAddress("read");
+            m_fluxengine.start();
+            break;
+        case 1:
+//           if (m_fluxengine.busy())
+//                m_fluxengine.stop();
+            m_fluxengine.setAddress("write");
+            m_fluxengine.start();
+            break;
+        case 2:
+            {
+                QString m_address = m_fluxengine.getAddress();
+                foreach (QString x, formats)
+                {
+                    qInfo() << Q_FUNC_INFO;
+                    qInfo() << m_address + " " + x + " -C";
+//                    if (m_fluxengine.busy())
+//                         m_fluxengine.stop();
+                    m_fluxengine.setAddress(m_address + " " + x + " -C");
+                    m_fluxengine.start();
+                }
+
+            break;
+            }
+        default: //
+            break;
+    }
+}
+
+QStringList DialogPreferences::initializeformats(QString data)
+    {
+        QStringList Formats;
+        //returns a stringlist with read or write formats.
+        QSettings settings("Fluxengine_GUI", "Fluxengine_GUI");
+        qInfo() << data;
+       if (data.contains("syntax: fluxengine read ") || (data.contains("syntax: fluxengine write ")))
+        {
+           /*syntax: fluxengine read <profile> [<options>...]
+            Use --help for option help.
+            Available profiles include:
+              acornadfs
+              acorndfs
+              aeslanier
+              amiga
+              ampro
+              apple2
+              atarist
+              brother
+              commodore1541
+              commodore1581
+              eco1
+              f85
+              fb100
+              ibm
+              macintosh
+              micropolis
+              mx
+              tids990
+              victor9k
+              zilogmcz
+            Or use a text file containing your own configuration.
+            */
+            qInfo() << Q_FUNC_INFO;
+
+            //get the readformats and store in section
+            int i = data.indexOf(":",0);
+            int j = data.indexOf(":", i+1);
+            for (int x=0; x < data.count(); x++)
+            {
+                i = data.indexOf("\n", j);
+                j = data.indexOf("\n", i+1);
+                if (j == -1) break;
+                qInfo() << j;
+                qInfo() << data.mid(i, j+1 - i).trimmed();
+                Formats.append(data.mid(i, j+1 - i).trimmed());
+                //i = j+1;
+
+            }
+            return Formats;
+    //        foreach (QString x, Formats) {
+    //            qInfo() << x;
+    //        }
+            //get the config for each readformat and store in section
+            /*
+             * input {
+                  flux {
+                    drive {
+                    }
+                  }
+                }
+                output {
+                  image {
+                    filename: "acornadfs.img"
+                    img {
+                    }
+                  }
+                }
+                decoder {
+                  ibm {
+                    sector_id_base: 0
+                  }
+                }
+                cylinders {
+                  start: 0
+                  end: 79
+                }
+                heads {
+                  start: 0
+                  end: 1
+                }
+                */
+            foreach (QString x, Formats) {
+                qInfo() << x;
+            }
+        }
+       return Formats;
+
+}
+
+QStringList DialogPreferences::getConfig(QString data)
+{
+    QStringList Configs;
+    struct FormatsDescription
+    {
+        QString strType;
+        QString strDescription;
+        QString strFilter;
+        QString strDefaultfilenaam;
+        QString trackstart;
+        QString trackstop;
+        QString headstart;
+        QString headstop;
+    };
+
+    if (data.contains("encoder") || (data.contains("decoder")))
+    {
+        int i = data.indexOf(":",0);
+        int j;
+//        for (int x=0; x < data.count(); x++)
+//        {
+//            i = data.indexOf("\n", );
+            j = data.indexOf("\n", i+1);
+//            if (j == -1) break;
+            qInfo() << j;
+            qInfo() << data.mid(i, j+1 - i).trimmed();
+            Configs.append(data.mid(i, j+1 - i).trimmed());
+            //i = j+1;
+
+//        }
+        return Configs;
+//        foreach (QString x, Configs) {
+//            qInfo() << x;
+//        }
+
+    }
+    //get the config for each readformat and store in section
+    /*
+     * input {
+          flux {
+            drive {
+            }
+          }
+        }
+        output {
+          image {
+            filename: "acornadfs.img"
+            img {
+            }
+          }
+        }
+        decoder {
+          ibm {
+            sector_id_base: 0
+          }
+        }
+        cylinders {
+          start: 0
+          end: 79
+        }
+        heads {
+          start: 0
+          end: 1
+        }
+        */
+    //stringlist contains filename, start cylinder, stop cylinder, start Head, stop head.
+    return Configs;
+}
+
+void DialogPreferences::output(QString data)
+{
+    if (data != "")
+    {
+        if (m_fluxengine.getAddress().contains("read") || m_fluxengine.getAddress().contains("write"))
+        {
+            formats = initializeformats(data);
+            //save formats somewhere
+        }
+        if (m_fluxengine.getAddress().contains("-C"))
+        {
+            qInfo() << Q_FUNC_INFO;
+            formats = getConfig(data);
+        }
+
+    //    foreach (QString x, formats) {
+    //        qInfo() << Q_FUNC_INFO;
+    //        QString m_address = m_fluxengine.getAddress();
+
+    //        m_address = m_address + x + " -C";
+    //        qInfo() << m_address;
+    //        m_fluxengine.setAddress(m_address);
+    //        m_fluxengine.start();
+    //    }
+
+        State = State + 1; //Data from fluxengine is processed.
+//        m_fluxengine.stop();
+        initializefluxengine();
+   }
 }
 
 void DialogPreferences::save()
--- dialogpreferences.h
+++ dialogpreferences.h
@@ -2,6 +2,7 @@
 #define DIALOGPREFERENCES_H
 
 #include <QDialog>
+#include <fluxengine.h>
 
 namespace Ui {
 class DialogPreferences;
@@ -9,8 +10,7 @@ class DialogPreferences;
 
 class DialogPreferences : public QDialog
 {
-    Q_OBJECT
-
+    Q_OBJECT 
 public:
     explicit DialogPreferences(QWidget *parent = nullptr);
     ~DialogPreferences();
@@ -18,11 +18,18 @@ public:
 private slots:
     void browse();
     void browseflux();
-
     void save();
     void browsefluxengine();
+    void initializefluxengine();
+    void output(QString data);
+    void enablecommands(bool running);
 private:
     Ui::DialogPreferences *ui;
+    fluxengine m_fluxengine;
+    int State;
+    QStringList formats;
+    QStringList initializeformats(QString data);
+    QStringList getConfig(QString data);
 };
 
 #endif // DIALOGPREFERENCES_H
