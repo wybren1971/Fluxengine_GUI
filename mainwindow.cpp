c28492669eeff78d1ebc4d54c4bd1e93ceaf2cbb
wybren1971 <wybren@vanduinenotten.nl>
2021-06-20 21:02:04 +0200

    Show which csv file is analyzed in windowtitle

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -704,7 +704,7 @@ void MainWindow::on_btnAnalyse_clicked()
     {
         //show the resulting png
         showlayout *form = new showlayout();
-        form->setWindowTitle("Visual layout of the csv: " + dir.absolutePath());
+        form->setWindowTitle("Visual layout of the csv: " + dir.absolutePath() + "/disklayout.png");
         //we have to wait for fluxengine to finish...
         form->LoadFile("disklayout.png");
         form->exec();
